d189e8d5ebaadbc8f97222b2d34e610c6cdae2b3
Nikolai Kondrashov <spbnick@gmail.com>
2009-04-05 20:38:46 +0000

    Continued describing HID report descriptor items.

--- include/item.h
+++ include/item.h
@@ -16,187 +16,88 @@
 extern "C" {
 #endif
 
-typedef enum hidrd_item_type {
-    HIDRD_ITEM_TYPE_MAIN    = 0,
-    HIDRD_ITEM_TYPE_GLOBAL,
-    HIDRD_ITEM_TYPE_LOCAL
-} hidrd_item_type;
-
 typedef struct hidrd_item {
-    hidrd_item_type type;
+    bool    long_item;
 } hidrd_item;
 
+/*
+ * Short item
+ */
+typedef enum hidrd_item_short_type {
+    HIDRD_ITEM_SHORT_TYPE_MAIN    = 0,
+    HIDRD_ITEM_SHORT_TYPE_GLOBAL,
+    HIDRD_ITEM_SHORT_TYPE_LOCAL
+} hidrd_item_short_type;
 
-/***********************************************************
- * Main item
- ***********************************************************/
-typedef enum hidrd_item_main_tag {
-    HIDRD_ITEM_MAIN_TAG_INPUT           = 0x8,
-    HIDRD_ITEM_MAIN_TAG_OUTPUT          = 0x9,
-    HIDRD_ITEM_MAIN_TAG_COLLECTION      = 0xA
-    HIDRD_ITEM_MAIN_TAG_FEATURE         = 0xB,
-    HIDRD_ITEM_MAIN_TAG_END_COLLECTION  = 0xC,
-} hidrd_item_main_tag;
+#define HIDRD_ITEM_SHORT_TYPE_KNOWN_MAX HIDRD_ITEM_SHORT_TYPE_LOCAL
+#define HIDRD_ITEM_SHORT_TYPE_RESERVED  0x3
+#define HIDRD_ITEM_SHORT_TYPE_MAX   HIDRD_ITEM_SHORT_TYPE_RESERVED
 
-typedef struct hidrd_item_main {
-    hidrd_item          item;
-    hidrd_item_main_tag tag;
-} hidrd_item_main;
+static inline bool
+hidrd_item_short_type_valid(hidrd_item_short_type type)
+{
+    return (type <= HIDRD_ITEM_SHORT_TYPE_MAX);
+}
 
-typedef struct hidrd_item_input {
-    hidrd_item_main item_main;
-    bool            constant;       /**< Data/Constant */
-    bool            variable;       /**< Array/Variable */
-    bool            relative;       /**< Absolute/Relative */
-    bool            wrap;           /**< No Wrap/Wrap */
-    bool            non_linear;     /**< Linear/Non Linear */
-    bool            no_preferred;   /**< Preferred State/No Preferred */
-    bool            null_state;     /**< No Null position/Null state */
-    bool            buffered_bytes; /**< Bit Field/Buffered Bytes */
-} hidrd_item_input;
+static inline bool
+hidrd_item_short_type_known(hidrd_item_short_type type)
+{
+    assert(hidrd_item_short_type_valid(type));
+    return (type <= HIDRD_ITEM_SHORT_TYPE_KNOWN_MAX);
+}
 
-typedef struct hidrd_item_output {
-    hidrd_item_main item_main;
-    bool            constant;       /**< Data/Constant */
-    bool            variable;       /**< Array/Variable */
-    bool            relative;       /**< Absolute/Relative */
-    bool            wrap;           /**< No Wrap/Wrap */
-    bool            non_linear;     /**< Linear/Non Linear */
-    bool            no_preferred;   /**< Preferred State/No Preferred */
-    bool            null_state;     /**< No Null position/Null state */
-    bool            is_volatile;    /**< Non volatile/volatile */
-    bool            buffered_bytes; /**< Bit Field/Buffered Bytes */
-} hidrd_item_output;
+static inline bool
+hidrd_item_short_type_reserved(hidrd_item_short_type type)
+{
+    assert(hidrd_item_short_type_valid(type));
+    return (type == HIDRD_ITEM_SHORT_TYPE_RESERVED);
+}
 
-typedef enum hidrd_item_coll_type {
-    HIDRD_ITEM_COLL_TYPE_PHYSICAL       = 0,
-    HIDRD_ITEM_COLL_TYPE_APPLICATION,
-    HIDRD_ITEM_COLL_TYPE_LOGICAL,
-    HIDRD_ITEM_COLL_TYPE_REPORT,
-    HIDRD_ITEM_COLL_TYPE_NAMED_ARRAY,
-    HIDRD_ITEM_COLL_TYPE_USAGE_SWITCH,
-    HIDRD_ITEM_COLL_TYPE_USAGE_MODIFIER
-} hidrd_item_coll_type;
-
-typedef struct hidrd_item_coll {
-    hidrd_item_main         item_main;
-    hidrd_item_coll_type    type;
-} hidrd_item_coll;
-
-typedef hidrd_item_main hidrd_item_end_coll;
-
-/***********************************************************
- * Global item
- ***********************************************************/
-typedef enum hidrd_item_global_tag {
-    HIDRD_ITEM_GLOBAL_TAG_USAGE_PAGE        = 0,
-    HIDRD_ITEM_GLOBAL_TAG_LOGICAL_MINIMUM,
-    HIDRD_ITEM_GLOBAL_TAG_LOGICAL_MAXIMUM,
-    HIDRD_ITEM_GLOBAL_TAG_PHYSICAL_MINIMUM,
-    HIDRD_ITEM_GLOBAL_TAG_PHYSICAL_MAXIMUM,
-    HIDRD_ITEM_GLOBAL_TAG_UNIT_EXPONENT,
-    HIDRD_ITEM_GLOBAL_TAG_UNIT,
-    HIDRD_ITEM_GLOBAL_TAG_REPORT_SIZE,
-    HIDRD_ITEM_GLOBAL_TAG_REPORT_ID,
-    HIDRD_ITEM_GLOBAL_TAG_REPORT_COUNT,
-    HIDRD_ITEM_GLOBAL_TAG_PUSH,
-    HIDRD_ITEM_GLOBAL_TAG_POP
-} hidrd_item_global_tag;
-
-typedef struct hidrd_item_global {
+typedef struct hidrd_item_short {
     hidrd_item              item;
-    hidrd_item_global_tag   tag;
-} hidrd_item_global;
+    hidrd_item_short_type   type;
+} hidrd_item_short;
 
-typedef struct hidrd_item_usage_page {
-    hidrd_item_global   item_global;
-    uint16_t            page;
-} hidrd_item_usage_page;
+static inline bool
+hidrd_item_short_valid(const hidrd_item_short *item_short)
+{
+    return !item_short->item.long_item &&
+           hidrd_item_short_type_valid(item_short->type);
+}
 
-typedef struct _hidrd_item_extent {
-    hidrd_item_global   item_global;
-    int32_t             value;
-} _hidrd_item_extent;
+/*
+ * Long item
+ */
+typedef struct hidrd_item_long {
+    hidrd_item  item;
+    uint8_t     tag;
+    uint8_t     size;
+    uint8_t     data[255];
+}
 
-typedef _hidrd_item_extent  hidrd_item_logical_minimum;
-typedef _hidrd_item_extent  hidrd_item_logical_maximum;
-typedef _hidrd_item_extent  hidrd_item_physical_minimum;
-typedef _hidrd_item_extent  hidrd_item_physical_maximum;
+static inline bool
+hidrd_item_long_valid(const hidrd_item_long *item_long)
+{
+    return item_long->item.long_item;
+}
 
-typedef struct hidrd_item_unit_exp {
-    hidrd_item_global   item_global;
-    int8_t              value;
-} hidrd_item_unit_exponent;
-
-typedef enum hidrd_item_unit_system {
-    HIDRD_ITEM_UNIT_SI_LINEAR           = 0x1,
-    HIDRD_ITEM_UNIT_SI_ROTATION         = 0x2,
-    HIDRD_ITEM_UNIT_ENGLISH_LINEAR      = 0x3,
-    HIDRD_ITEM_UNIT_ENGLISH_ROTATION    = 0x4
-} hidrd_item_unit_system;
-
-typedef struct hidrd_item_unit_type_exp {
-    int8_t  length
-    int8_t  mass;
-    int8_t  time;
-    int8_t  temperature;
-    int8_t  current;
-    int8_t  luminous_intensity;
-} hidrd_item_unit_exp;
-
-typedef struct hidrd_item_unit {
-    hidrd_item_global           item_global;
-    hidrd_item_unit_system      system;
-    hidrd_item_unit_type_exp    type_exp;
-} hidrd_item_unit;
-
-typedef struct _hidrd_item_global_uint {
-    hidrd_item_global   item_global;
-    uint32_t            value;
-} _hidrd_item_global_uint;
-
-typedef _hidrd_item_global_uint hidrd_item_report_size;
-typedef _hidrd_item_global_uint hidrd_item_report_count;
-
-typedef hidrd_item_global   hidrd_item_push;
-typedef hidrd_item_global   hidrd_item_pop;
 
 /***********************************************************
- * Local item
+ * Reserved item type
  ***********************************************************/
-typedef enum hidrd_item_local_tag {
-    HIDRD_ITEM_LOCAL_TAG_USAGE                  = 0,
-    HIDRD_ITEM_LOCAL_TAG_USAGE_MINIMUM,
-    HIDRD_ITEM_LOCAL_TAG_USAGE_MAXIMUM,
-    HIDRD_ITEM_LOCAL_TAG_DESIGNATOR_INDEX,
-    HIDRD_ITEM_LOCAL_TAG_DESIGNATOR_MINIMUM,
-    HIDRD_ITEM_LOCAL_TAG_DESIGNATOR_MAXIMUM,
-    HIDRD_ITEM_LOCAL_TAG_STRING_INDEX,
-    HIDRD_ITEM_LOCAL_TAG_STRING_MINIMUM,
-    HIDRD_ITEM_LOCAL_TAG_STRING_MAXIMUM,
-    HIDRD_ITEM_LOCAL_TAG_DELIMITER
-} hidrd_item_global_tag;
+typedef struct hidrd_item_short_reserved {
+    hidrd_item_short    item_short;
+    uint8_t             tag;
+    uint32_t            data;
+} hidrd_item_short_reserved;
 
-typedef struct hidrd_item_local {
-    hidrd_item              item;
-    hidrd_item_local_tag    tag;
-} hidrd_item_local;
-
-typedef struct _hidrd_item_local_uint {
-    hidrd_item_local    item_local;
-    uint32_t            value;
-} _hidrd_item_local_uint;
-
-typedef _hidrd_item_local_uint  hidrd_item_usage;
-typedef _hidrd_item_local_uint  hidrd_item_usage_minimum;
-typedef _hidrd_item_local_uint  hidrd_item_usage_maximum;
-typedef _hidrd_item_local_uint  hidrd_item_designator_index;
-typedef _hidrd_item_local_uint  hidrd_item_designator_minimum;
-typedef _hidrd_item_local_uint  hidrd_item_designator_maximum;
-typedef _hidrd_item_local_uint  hidrd_item_string_index;
-typedef _hidrd_item_local_uint  hidrd_item_string_minimum;
-typedef _hidrd_item_local_uint  hidrd_item_string_maximum;
-typedef _hidrd_item_local_uint  hidrd_item_delimiter;
+static inline bool
+hidrd_item_short_reserved_valid(const hidrd_item_short_reserved *reserved)
+{
+    return hidrd_item_short_valid(&reserved->item_short) &&
+           hidrd_item_short_type_reserved(reserved->item_short.type) &&
+           (reserved->tag < 0x10);
+}
 
 #ifdef __cplusplus
 } /* extern "C" */
---
+++ include/item_global.h
@@ -0,0 +1,119 @@
+/** @file
+ * @brief HID report descriptor - global item
+ *
+ * Copyright (C) 2009 Nikolai Kondrashov
+ *
+ * @author Nikolai Kondrashov <spbnick@gmail.com>
+ *
+ * @(#) $Id: $
+ */
+
+
+#ifndef __HIDRD_ITEM_GLOBAL_H__
+#define __HIDRD_ITEM_GLOBAL_H__
+
+#include "hidrd/item.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum hidrd_item_global_tag {
+    HIDRD_ITEM_GLOBAL_TAG_USAGE_PAGE        = 0,
+    HIDRD_ITEM_GLOBAL_TAG_LOGICAL_MINIMUM,
+    HIDRD_ITEM_GLOBAL_TAG_LOGICAL_MAXIMUM,
+    HIDRD_ITEM_GLOBAL_TAG_PHYSICAL_MINIMUM,
+    HIDRD_ITEM_GLOBAL_TAG_PHYSICAL_MAXIMUM,
+    HIDRD_ITEM_GLOBAL_TAG_UNIT_EXPONENT,
+    HIDRD_ITEM_GLOBAL_TAG_UNIT,
+    HIDRD_ITEM_GLOBAL_TAG_REPORT_SIZE,
+    HIDRD_ITEM_GLOBAL_TAG_REPORT_ID,
+    HIDRD_ITEM_GLOBAL_TAG_REPORT_COUNT,
+    HIDRD_ITEM_GLOBAL_TAG_PUSH,
+    HIDRD_ITEM_GLOBAL_TAG_POP
+} hidrd_item_global_tag;
+
+typedef struct hidrd_item_global {
+    hidrd_item_short        item_short;
+    hidrd_item_global_tag   tag;
+} hidrd_item_global;
+
+/*
+ * Usage page
+ */
+typedef struct hidrd_item_usage_page {
+    hidrd_item_global   item_global;
+    uint16_t            page;
+} hidrd_item_usage_page;
+
+/*
+ * Logical and physical minimum/maximum
+ */
+typedef struct _hidrd_item_extent {
+    hidrd_item_global   item_global;
+    int32_t             value;
+} _hidrd_item_extent;
+
+typedef _hidrd_item_extent  hidrd_item_logical_minimum;
+typedef _hidrd_item_extent  hidrd_item_logical_maximum;
+typedef _hidrd_item_extent  hidrd_item_physical_minimum;
+typedef _hidrd_item_extent  hidrd_item_physical_maximum;
+
+/*
+ * Unit exponent
+ */
+typedef struct hidrd_item_unit_exp {
+    hidrd_item_global   item_global;
+    int8_t              value;
+} hidrd_item_unit_exponent;
+
+/*
+ * Unit
+ */
+typedef enum hidrd_item_unit_system {
+    HIDRD_ITEM_UNIT_SI_LINEAR           = 0x1,
+    HIDRD_ITEM_UNIT_SI_ROTATION         = 0x2,
+    HIDRD_ITEM_UNIT_ENGLISH_LINEAR      = 0x3,
+    HIDRD_ITEM_UNIT_ENGLISH_ROTATION    = 0x4
+} hidrd_item_unit_system;
+
+typedef struct hidrd_item_unit_type_exp {
+    int8_t  length
+    int8_t  mass;
+    int8_t  time;
+    int8_t  temperature;
+    int8_t  current;
+    int8_t  luminous_intensity;
+} hidrd_item_unit_exp;
+
+typedef struct hidrd_item_unit {
+    hidrd_item_global           item_global;
+    hidrd_item_unit_system      system;
+    hidrd_item_unit_type_exp    type_exp;
+} hidrd_item_unit;
+
+/*
+ * Report size and count
+ */
+typedef struct _hidrd_item_global_uint {
+    hidrd_item_global   item_global;
+    uint32_t            value;
+} _hidrd_item_global_uint;
+
+typedef _hidrd_item_global_uint hidrd_item_report_size;
+typedef _hidrd_item_global_uint hidrd_item_report_count;
+
+/*
+ * Push and pop
+ */
+typedef hidrd_item_global   hidrd_item_push;
+typedef hidrd_item_global   hidrd_item_pop;
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __HIDRD_ITEM_GLOBAL_H__ */
+
+
+
