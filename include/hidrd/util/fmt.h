292b41b955a1d94c386ee014e9d7a054ae91d2a3
Nikolai Kondrashov <spbnick@gmail.com>
2010-03-20 12:40:17 +0000

    Added fmt utility library.

---
+++ include/hidrd/util/fmt.h
@@ -0,0 +1,68 @@
+/** @file
+ * @brief HID report descriptor - utilities - "universal" string formatting
+ *
+ * Copyright (C) 2010 Nikolai Kondrashov
+ *
+ * This file is part of hidrd.
+ *
+ * Hidrd is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hidrd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hidrd; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @author Nikolai Kondrashov <spbnick@gmail.com>
+ *
+ * @(#) $Id$
+ */
+
+#ifndef __HIDRD_UTIL_FMT_H__
+#define __HIDRD_UTIL_FMT_H__
+
+#include <stdbool.h>
+#include <stdarg.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Formatting type */
+typedef enum hidrd_fmt_type {
+    HIDRD_FMT_TYPE_NULL,    /**< NULL string */
+    HIDRD_FMT_TYPE_S32,     /**< Signed 32-bit integer */
+    HIDRD_FMT_TYPE_U32,     /**< Unsigned 32-bit integer */
+    HIDRD_FMT_TYPE_STRDUP,  /**< String duplication */
+    HIDRD_FMT_TYPE_STROWN,  /**< String ownership taking */
+    HIDRD_FMT_TYPE_HEX      /**< Hex string */
+} hidrd_fmt_type;
+
+/**
+ * Format a dynamically allocated string according to format type; va_list
+ * pointer version.
+ *
+ * @param pstr  Location for a (dynamically allocated) resulting string
+ *              pointer.
+ * @param type  Format type.
+ * @param pap   Pointer to a va_list containing format arguments.
+ *
+ * @return True if formatted successfully, false otherwise.
+ *
+ * @sa hidrdm_fmt_type
+ */
+extern bool hidrd_fmtpva(char             **pstr,
+                         hidrd_fmt_type     type,
+                         va_list           *pap);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __HIDRD_UTIL_FMT_H__ */
---
+++ lib/util/fmt.c
@@ -0,0 +1,102 @@
+/** @file
+ * @brief HID report descriptor - utilities - string formatting
+ *
+ * Copyright (C) 2010 Nikolai Kondrashov
+ *
+ * This file is part of hidrd.
+ *
+ * Hidrd is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hidrd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hidrd; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @author Nikolai Kondrashov <spbnick@gmail.com>
+ *
+ * @(#) $Id$
+ */
+
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include "hidrd/util/dec.h"
+#include "hidrd/util/hex.h"
+#include "hidrd/util/fmt.h"
+
+
+bool
+hidrd_fmtpva(char             **pstr,
+             hidrd_fmt_type     type,
+             va_list           *pap)
+{
+    char       *str;
+
+    switch (type)
+    {
+        case HIDRD_FMT_TYPE_NULL:
+            str = NULL;
+            break;
+        case HIDRD_FMT_TYPE_S32:
+            str = hidrd_dec_s32_to_str(va_arg(*pap, int32_t));
+            if (str == NULL)
+                return false;
+            break;
+        case HIDRD_FMT_TYPE_U32:
+            str = hidrd_dec_u32_to_str(va_arg(*pap, uint32_t));
+            if (str == NULL)
+                return false;
+            break;
+        case HIDRD_FMT_TYPE_STRDUP:
+            {
+                const char *arg = va_arg(*pap, const char *);
+
+                assert(arg != NULL);
+
+                str = strdup(arg);
+                if (str == NULL)
+                    return false;
+            }
+            break;
+        case HIDRD_FMT_TYPE_STROWN:
+            {
+                char       *arg = va_arg(*pap, char *);
+
+                assert(arg != NULL);
+
+                str = arg;
+            }
+            break;
+        case HIDRD_FMT_TYPE_HEX:
+            {
+                void   *buf     = va_arg(*pap, void *);
+                size_t  size    = va_arg(*pap, size_t);
+
+                assert(buf != NULL || size == 0);
+
+                str = hidrd_hex_buf_to_str(buf, size);
+                if (str == NULL)
+                    return false;
+            }
+            break;
+        default:
+            assert(!"Unknown string format");
+            return false;
+    }
+
+    if (pstr != NULL)
+        *pstr = str;
+    else
+        free(str);
+
+    return true;
+}
+
+
