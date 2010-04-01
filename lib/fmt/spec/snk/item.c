f8877b4961c191303e22b52addf2b15eb5cde81b
Nikolai Kondrashov <spbnick@gmail.com>
2010-04-01 08:45:39 +0000

    Added a few simple items support to spec sink.

--- lib/fmt/spec/snk/item.c
+++ lib/fmt/spec/snk/item.c
@@ -24,6 +24,7 @@
  * @(#) $Id$
  */
 
+#include "hidrd/util/str.h"
 #include "hidrd/item.h"
 #include "item_ent.h"
 #include "item.h"
@@ -37,6 +38,21 @@
 
 #define COMMENT(_fmt, _args...) \
     SPEC_SNK_ITEM_ENT_NT_COMMENT, HIDRD_FMT_TYPE_##_fmt, ##_args
+
+#define CASE_ITEM_S32(_TYPE, _NAME, _name) \
+    case HIDRD_ITEM_##_TYPE##_TAG_##_NAME:                              \
+        return                                                          \
+            ITEM(_name,                                                 \
+                 VALUE(S32,                                             \
+                       (int32_t)hidrd_item_##_name##_get_value(item)))
+
+#define CASE_ITEM_U32(_TYPE, _NAME, _name) \
+    case HIDRD_ITEM_##_TYPE##_TAG_##_NAME:                              \
+        return                                                          \
+            ITEM(_name,                                                 \
+                 VALUE(U32,                                             \
+                       (uint32_t)hidrd_item_##_name##_get_value(item)))
+
 
 static bool
 spec_snk_item_main(hidrd_spec_snk_inst *spec_snk,
@@ -77,6 +93,64 @@ spec_snk_item_global(hidrd_spec_snk_inst *spec_snk,
 
     switch (hidrd_item_global_get_tag(item))
     {
+        CASE_ITEM_S32(GLOBAL, LOGICAL_MINIMUM, logical_minimum);
+        CASE_ITEM_S32(GLOBAL, LOGICAL_MAXIMUM, logical_maximum);
+        CASE_ITEM_S32(GLOBAL, PHYSICAL_MINIMUM, physical_minimum);
+        CASE_ITEM_S32(GLOBAL, PHYSICAL_MAXIMUM, physical_maximum);
+        CASE_ITEM_S32(GLOBAL, UNIT_EXPONENT, unit_exponent);
+        CASE_ITEM_U32(GLOBAL, REPORT_SIZE, report_size);
+        CASE_ITEM_U32(GLOBAL, REPORT_ID, report_id);
+        CASE_ITEM_U32(GLOBAL, REPORT_COUNT, report_count);
+
+        case HIDRD_ITEM_GLOBAL_TAG_USAGE_PAGE:
+            spec_snk->state->usage_page =
+                hidrd_item_usage_page_get_value(item);
+            return
+                ITEM(usage_page,
+                     VALUE(STROWN,
+                           hidrd_tkn_hmnz(
+                            hidrd_usage_page_to_token_or_hex(
+                                hidrd_item_usage_page_get_value(item)),
+                            HIDRD_TKN_HMNZ_CAP_WF)),
+                     COMMENT(STROWN,
+                             hidrd_str_uc_first(
+                                hidrd_usage_page_desc(
+                                    hidrd_item_usage_page_get_value(
+                                        item)))));
+
+        case HIDRD_ITEM_GLOBAL_TAG_PUSH:
+            {
+                hidrd_spec_snk_state   *new_state;
+
+                if (!ITEM(push))
+                    return false;
+
+                /* Push state */
+                new_state = malloc(sizeof(*new_state));
+                if (new_state == NULL)
+                    return false;
+                memcpy(new_state, spec_snk->state, sizeof(*new_state));
+                new_state->prev = spec_snk->state;
+                spec_snk->state = new_state;
+
+                return true;
+            }
+
+        case HIDRD_ITEM_GLOBAL_TAG_POP:
+            {
+                hidrd_spec_snk_state   *prev_state;
+
+                /* Pop state, if possible */
+                prev_state = spec_snk->state->prev;
+                if (prev_state != NULL)
+                {
+                    free(spec_snk->state);
+                    spec_snk->state = prev_state;
+                }
+
+                return ITEM(pop);
+            }
+
         default:
             /* TODO add contents value */
             return ITEM(global);
@@ -92,6 +166,21 @@ spec_snk_item_local(hidrd_spec_snk_inst *spec_snk,
 
     switch (hidrd_item_local_get_tag(item))
     {
+        CASE_ITEM_U32(LOCAL, DESIGNATOR_INDEX, designator_index);
+        CASE_ITEM_U32(LOCAL, DESIGNATOR_MINIMUM, designator_minimum);
+        CASE_ITEM_U32(LOCAL, DESIGNATOR_MAXIMUM, designator_maximum);
+        CASE_ITEM_U32(LOCAL, STRING_INDEX, string_index);
+        CASE_ITEM_U32(LOCAL, STRING_MINIMUM, string_minimum);
+        CASE_ITEM_U32(LOCAL, STRING_MAXIMUM, string_maximum);
+
+        case HIDRD_ITEM_LOCAL_TAG_DELIMITER:
+            return ITEM(delimiter,
+                        VALUE(STRDUP,
+                              (hidrd_item_delimiter_get_value(item) ==
+                               HIDRD_ITEM_DELIMITER_SET_OPEN)
+                                ? "Open"
+                                : "Close"));
+
         default:
             /* TODO add contents value */
             return ITEM(local);
