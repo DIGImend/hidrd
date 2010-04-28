bab9143c634bbd674be979a0ed5d7ef4a44675a0
Nikolai Kondrashov <spbnick@gmail.com>
2010-04-28 09:08:08 +0000

    Moved usage page and ID database code to separate files.
    THE BUILD IS STILL BROKEN.

---
+++ include/hidrd/usage/id_desc.h
@@ -0,0 +1,62 @@
+/** @file
+ * @brief HID report descriptor - usage ID description
+ *
+ * Copyright (C) 2010 Nikolai Kondrashov
+ *
+ * This file is part of hidrd.
+ *
+ * Hidrd is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hidrd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hidrd; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @author Nikolai Kondrashov <spbnick@gmail.com>
+ *
+ * @(#) $Id: id.h 331 2010-04-01 20:05:18Z spb_nick $
+ */
+
+#ifndef __HIDRD_USAGE_ID_DESC_H__
+#define __HIDRD_USAGE_ID_DESC_H__
+
+#include "hidrd/usage/id.h"
+#include "hidrd/usage/type.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Usage ID description */
+typedef struct hidrd_usage_id_desc {
+    hidrd_usage_id          value;      /**< Usage ID value */
+    hidrd_usage_type_set    type_set;   /**< Type set (bitmask) */
+#ifdef HIDRD_WITH_TOKENS
+    const char             *token;      /**< Token */
+#endif
+#ifdef HIDRD_WITH_NAMES
+    const char             *name;       /**< Name */
+#endif
+} hidrd_usage_id_desc;
+
+/**
+ * Check if a usage ID description is valid.
+ *
+ * @param desc  Usage ID description to check.
+ *
+ * @return True if the description is valid, false otherwise.
+ */
+extern bool hidrd_usage_id_desc_valid(const hidrd_usage_id_desc *desc);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __HIDRD_USAGE_ID_DESC */
---
+++ lib/usage/page_desc.c
@@ -0,0 +1,46 @@
+/** @file
+ * @brief HID report descriptor - usage page description
+ *
+ * Copyright (C) 2010 Nikolai Kondrashov
+ *
+ * This file is part of hidrd.
+ *
+ * Hidrd is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hidrd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hidrd; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @author Nikolai Kondrashov <spbnick@gmail.com>
+ *
+ * @(#) $Id: id.h 331 2010-04-01 20:05:18Z spb_nick $
+ */
+
+#include "hidrd/util/tkn.h"
+#include "hidrd/usage/id_desc_list.h"
+#include "hidrd/usage/page_desc.h"
+
+
+bool
+hidrd_usage_page_desc_valid(const hidrd_usage_page_desc *desc)
+{
+    return desc != NULL &&
+           hidrd_usage_page_valid(desc->value) &&
+#ifdef HIDRD_WITH_TOKENS
+           hidrd_tkn_valid(desc->token) &&
+#endif
+#ifdef HIDRD_WITH_NAMES
+           desc->name != NULL &&
+#endif
+           hidrd_usage_id_desc_list_valid(desc->id_list, desc->id_num);
+}
+
+
