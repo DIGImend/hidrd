3f7e9f80209ccbfb64a0cbef132b222a9569f7e0
Nikolai Kondrashov <spbnick@gmail.com>
2010-04-28 08:46:24 +0000

    Removed decimal number conversion functions, replaced with generic conversion function-using macros.
    THE BUILD IS STILL BROKEN.

--- include/hidrd/util/dec.h
+++ include/hidrd/util/dec.h
@@ -36,209 +36,29 @@ extern "C" {
 #endif
 
 /**
- * Convert a decimal string to an unsigned 32-bit integer.
+ * Convert a specified integer number type to a decimal string.
  *
- * @param pnum  Location for the converted number; could be NULL.
- * @param str   Decimal string to convert from.
+ * @param _t    Short integer number type name (u32/s32/u16/s16/u8/s8).
+ * @param _n    Number.
  *
- * @return True if the string was valid and converted successfully, false
+ * @return Dynamically allocated string, or NULL, if failed to allocate
+ *         memory.
+ */
+#define HIDRD_DEC_TO_STR(_t, _n) \
+    HIDRD_NUM_TO_STR(_t, _n, NONE, DEC)
+
+/**
+ * Convert a decimal string to a specified integer type.
+ *
+ * @param _t    Short integer number type name (u32/s32/u16/s16/u8/s8).
+ * @param _pn   Location for resulting number.
+ * @param _s    String to parse.
+ *
+ * @return True if the string was valid and parsed successfully, false
  *         otherwise.
  */
-static inline bool
-hidrd_dec_u32_from_str(uint32_t *pnum, const char *str)
-{
-    assert(str != NULL);
-    return hidrd_num_u32_from_str(pnum, str, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Convert an unsigned 32-bit integer to a decimal string.
- *
- * @param num   The number to convert.
- *
- * @return Dynamically allocated decimal string, or NULL if failed to
- *         allocate memory.
- */
-static inline char *
-hidrd_dec_u32_to_str(uint32_t num)
-{
-    return hidrd_num_u32_to_str(num, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Convert a decimal string to an unsigned 16-bit integer.
- *
- * @param pnum  Location for the converted number; could be NULL.
- * @param str   Decimal string to convert from.
- *
- * @return True if the string was valid and converted successfully, false
- *         otherwise.
- */
-static inline bool
-hidrd_dec_u16_from_str(uint16_t *pnum, const char *str)
-{
-    assert(str != NULL);
-    return hidrd_num_u16_from_str(pnum, str, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Convert an unsigned 16-bit integer to a decimal string.
- *
- * @param num   The number to convert.
- *
- * @return Dynamically allocated decimal string, or NULL if failed to
- *         allocate memory.
- */
-static inline char *
-hidrd_dec_u16_to_str(uint16_t num)
-{
-    return hidrd_num_u16_to_str(num, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Convert a decimal string to an unsigned 8-bit integer.
- *
- * @param pnum  Location for the converted number; could be NULL.
- * @param str   Decimal string to convert from.
- *
- * @return True if the string was valid and converted successfully, false
- *         otherwise.
- */
-static inline bool
-hidrd_dec_u8_from_str(uint8_t *pnum, const char *str)
-{
-    assert(str != NULL);
-    return hidrd_num_u8_from_str(pnum, str, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Convert an unsigned 8-bit integer to a decimal string.
- *
- * @param num   The number to convert.
- *
- * @return Dynamically allocated decimal string, or NULL if failed to
- *         allocate memory.
- */
-static inline char *
-hidrd_dec_u8_to_str(uint8_t num)
-{
-    return hidrd_num_u8_to_str(num, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Convert a decimal string to a signed 32-bit integer.
- *
- * @param pnum  Location for the converted number; could be NULL.
- * @param str   Decimal string to convert from.
- *
- * @return True if the string was valid and converted successfully, false
- *         otherwise.
- */
-static inline bool
-hidrd_dec_s32_from_str(int32_t *pnum, const char *str)
-{
-    assert(str != NULL);
-    return hidrd_num_s32_from_str(pnum, str, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Convert a signed 32-bit integer to a decimal string.
- *
- * @param num   The number to convert.
- *
- * @return Dynamically allocated decimal string, or NULL if failed to
- *         allocate memory.
- */
-static inline char *
-hidrd_dec_s32_to_str(int32_t num)
-{
-    return hidrd_num_s32_to_str(num, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Convert a decimal string to a signed 16-bit integer.
- *
- * @param pnum  Location for the converted number; could be NULL.
- * @param str   Decimal string to convert from.
- *
- * @return True if the string was valid and converted successfully, false
- *         otherwise.
- */
-static inline bool
-hidrd_dec_s16_from_str(int16_t *pnum, const char *str)
-{
-    assert(str != NULL);
-    return hidrd_num_s16_from_str(pnum, str, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Convert a signed 16-bit integer to a decimal string.
- *
- * @param num   The number to convert.
- *
- * @return Dynamically allocated decimal string, or NULL if failed to
- *         allocate memory.
- */
-static inline char *
-hidrd_dec_s16_to_str(int16_t num)
-{
-    return hidrd_num_s16_to_str(num, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Convert a decimal string to a signed 8-bit integer.
- *
- * @param pnum  Location for the converted number; could be NULL.
- * @param str   Decimal string to convert from.
- *
- * @return True if the string was valid and converted successfully, false
- *         otherwise.
- */
-static inline bool
-hidrd_dec_s8_from_str(int8_t *pnum, const char *str)
-{
-    assert(str != NULL);
-    return hidrd_num_s8_from_str(pnum, str, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Convert a signed 8-bit integer to a decimal string.
- *
- * @param num   The number to convert.
- *
- * @return Dynamically allocated decimal string, or NULL if failed to
- *         allocate memory.
- */
-static inline char *
-hidrd_dec_s8_to_str(int8_t num)
-{
-    return hidrd_num_s8_to_str(num, HIDRD_NUM_BASE_DEC);
-}
-
-/**
- * Declare a function pair for converting specified number type to from a
- * decimal string.
- *
- * @param _type     Long (global) name of the type being converted; will be
- *                  used in the function names.
- * @param _t        Short (local) name of the type being converted; will be
- *                  used for local variable names.
- */
-#define HIDRD_DEC_CONV_DECLS(_type, _t) \
-    HIDRD_NUM_CONV_DECLS(_type, _t, dec)
-
-/**
- * Define a function pair for converting specified number type to from a
- * decimal string.
- *
- * @param _type     Long (global) name of the type being converted; will be
- *                  used in the function names.
- * @param _t        Short (local) name of the type being converted; will be
- *                  used for local variable names.
- * @param _n        Short (convenience) name of the number type (like s8).
- */
-#define HIDRD_DEC_CONV_DEFS(_type, _t, _n) \
-    HIDRD_NUM_CONV_DEFS(_type, _t, _n, dec)
+#define HIDRD_DEC_FROM_STR(_t, _pn, _s) \
+    HIDRD_NUM_FROM_STR(_t, _pn, _s, NONE, DEC)
 
 #ifdef __cplusplus
 } /* extern "C" */
